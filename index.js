36443a3cfa42e864439f540c7cdfe717fda8cec4
Freecody15 <Dakota_pope1996@hotmail.com>
2022-08-29 11:24:16 -0400

    temp remove so myflix-cilent can access

--- index.js
+++ index.js
@@ -154,8 +154,8 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (r
       }
     });
 });
-//Get all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+//Get all movies temp removed passport.authenticate('jwt', { session: false })
+app.get('/movies', (req, res) => {
   movies.find()
     .then((movies) => {
       res.status(201).json(movies);
